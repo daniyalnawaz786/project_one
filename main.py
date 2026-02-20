87f56052382572c9b8bfd7488a6ce951bdcfd85b
Daniyal nawaz <nawazdaniyal97@gmail.com>
2026-02-20 15:52:25 +0500

    adding lgging funtion in project

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.datascience import logger
+
+if __name__ == "__main__":
+    logger.info("Application started")
+    logger.info("This is a test log message")
+    logger.info("Thies is first time i am tryin logging fuction and want to test it out an di also gonn aus eit in my main project")
+    
